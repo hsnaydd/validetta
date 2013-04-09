b4cb79d71b0eef771439f48eacace6c18e288521
Hasan Aydoğdu <hasan_aydogdu@windowslive.com>
2013-04-09 16:40:31 +0300

    Düzenleme & Geliştirme
    Düzenleme
    - Kodlar ingilizceye çevilirdi
    - reset fonksiyonu ile ilgili küçük bir düzenleme yapıldı
    Geliştirme
    - Ajax işlemi yapabilme özelliği kazandırıldı
    - Ajax çağırma dosyaları oluşturuldu

--- hsnValidate.js
+++ hsnValidate.js
@@ -1,5 +1,5 @@
 /*!
- * hsnLightBox - jQuery Eklentisi
+ * hsnValidate - jQuery Eklentisi
  * version: 1.0 (30 Mart 2013, Cumartesi)
  * @jQuery v1.7 ve üstü ile çalışmaktadır.
  *
@@ -15,144 +15,178 @@
 
 (function($){
 	var handler = false,obje,
-	ayarlar = {
-		hataClass 			:	'formHata',
-		hataKapaClass 		:	'formHataKapa',
-		hataMesaj			: 	{
-			bos			:	'Bu alanı doldurmanız gerekli. Lütfen kontrol ediniz.',
-			email		:	'Eposta adresiniz geçersiz görünüyor. Lütfen kontrol ediniz.',
-			numara		:	'Bu alana sadece rakam girişi yapabilirsiniz.',
-			checkbox	:	'Bu alanı işaretmeleniz gerekli. Lütfen kontrol ediniz.',
-			maxChecked	:	'En fazla {sayi} seçim yapabilirsiniz. Lütfen kontrol ediniz.',
-			minChecked	:	'En az {sayi} seçim yapmalısınız. Lütfen kontrol ediniz.'
+	defaults = {
+		errorClass 		: 'formHata',
+		errorCloseClass : 'formHataKapa',
+		errorMessage : {
+			empty		: 'Bu alanı doldurmanız gerekli. Lütfen kontrol ediniz.',
+			email		: 'Eposta adresiniz geçersiz görünüyor. Lütfen kontrol ediniz.',
+			number		: 'Bu alana sadece rakam girişi yapabilirsiniz.',
+			checkbox	: 'Bu alanı işaretmeleniz gerekli. Lütfen kontrol ediniz.',
+			maxChecked	: 'En fazla {count} seçim yapabilirsiniz. Lütfen kontrol ediniz.',
+			minChecked	: 'En az {count} seçim yapmalısınız. Lütfen kontrol ediniz.'
 		},
-		checkbox 			:	{
-			name	:	null,
+		checkbox : {
+			name	: null,
 			max		: null,
 			min		: null		
-		}, 
-		blurTetikleme		: 	true,
-		fonksiyon			: 	null	
+		},
+		ajax : {
+			call 		: false,
+			type		: 'GET',
+			url			: null,
+			dataType	: 'html',
+			beforeSend	: $.noop,
+			success 	: $.noop,
+			fail		: $.noop,
+			complete	: $.noop
+		},
+		blurTrigger		: true,
+		onCompleteFunc	: $.noop	
 	},
-	temizle = function(value){
+	clear = function(value){
 		return value = value.replace(/^\s+|\s+$/g, '');
 	},
-	kontrol = {
+	check = {
 		//Bosluk Kontrolu
-		bosluk : function(_input){
-				if(temizle($(_input).val()) == ''){
-				return pencere.ac(_input,ayarlar.hataMesaj.bos);
+		space : function(_input){
+				if(clear($(_input).val()) == ''){
+				return _window.open(_input,options.errorMessage.empty);
 				}else{return;}
 		},
 		//Mail Kontrolu
 		mail : function(_input){
 			//var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; 
 			var reg = new RegExp(/^[a-z]{1}[\d\w\.-]+@[\d\w-]{3,}\.[\w]{2,3}(\.\w{2})?$/);
-			if((reg.test($(_input).val()) == false) && ($(_input).val()!='')){return pencere.ac(_input,ayarlar.hataMesaj.email);}
+			if((reg.test($(_input).val()) == false) && ($(_input).val()!='')){return _window.open(_input,options.errorMessage.email);}
 			else{return;}
 		},
 		//Numara Kontrolu
-		numara : function(_input){
+		number : function(_input){
 			var reg = new RegExp(/^[\+][0-9]+?$|^[0-9]+?$/);
-			if((reg.test($(_input).val()) == false) && ($(_input).val()!='')){return pencere.ac(_input,ayarlar.hataMesaj.numara);}
+			if((reg.test($(_input).val()) == false) && ($(_input).val()!='')){return _window.open(_input,options.errorMessage.number);}
 			else{return;}
 		},
 		//Checkbox Kontrolu
 		checkbox : {
-			secim : function(_inp){
+			checked : function(_inp){
 						if (!$(_inp).is(':checked')){
-							return pencere.ac(_inp,ayarlar.hataMesaj.checkbox);
+							return _window.open(_inp,options.errorMessage.checkbox);
 						}else{return;}
 					},
 			limit  : function(_inp){
 						var max,min,
-						sayi = _inp.filter(':checked').length;
-						if((max = ayarlar.checkbox.max) && sayi > max){
-							return pencere.ac(_inp.eq(0),ayarlar.hataMesaj.maxChecked.replace('{sayi}',max));
-						}else if((min = ayarlar.checkbox.min) && sayi < min){
-							return pencere.ac(_inp.eq(0),ayarlar.hataMesaj.minChecked.replace('{sayi}',min));
+						count = _inp.filter(':checked').length;
+						if((max = options.checkbox.max) && count > max){
+							return _window.open(_inp.eq(0),options.errorMessage.maxChecked.replace('{count}',max));
+						}else if((min = options.checkbox.min) && count < min){
+							return _window.open(_inp.eq(0),options.errorMessage.minChecked.replace('{count}',min));
 						}else{return;}
 					}
 		}
 	},
-	pencere = {
-		ac : function(_input,hata){
+	_window = {
+		open : function(_input,error){
 			var pos= $(_input).position(),
-			gen = $(_input).width(),
-			yuk = $(_input).height(),
-			_top= pos.top,
-			hsnHataObje = $('<span>').addClass(ayarlar.hataClass),
-			hsnHataKapatObje = $('<span class="hsnHataKapat">x</span>');
-			hsnHataKapatObje.addClass(ayarlar.hataKapaClass);
-			hsnHataObje.empty().css({
-				'left':pos.left+gen+30+'px',
-				'top' :_top+'px'
+			W = $(_input).width(),
+			H = $(_input).height(),
+			T= pos.top,
+			errorObject = $('<span>').addClass(options.errorClass),
+			errorCloseObject = $('<span class="hsnHataKapat">x</span>');
+			errorCloseObject.addClass(options.errorCloseClass);
+			errorObject.empty().css({
+				'left':pos.left+W+30+'px',
+				'top' :T+'px'
 			});
-			$(_input).parent().append(hsnHataObje);
-			hsnHataObje.append(hata,hsnHataKapatObje);
+			$(_input).parent().append(errorObject);
+			errorObject.append(error,errorCloseObject);
 			handler = true; 
 		},
-		kapa : function(_input){
-			$(_input).parent().children('.'+ayarlar.hataClass+'').remove();
+		close : function(_input){
+			$(_input).parent().children('.'+options.errorClass+'').remove();
 			 handler = false;
 		}
 	},
-	basla = function(){
+	ajax = function(){
+		var data = $(obje).serialize(),request,
+			url = (options.ajax.url) ? options.ajax.url : $(obje).attr('action');
+		/* Debug */
+		if((!options.ajax.url && $(obje).attr('action')=='')) console.log('Form action not valid !');
+		$.ajax({
+			type 	: options.ajax.type,
+			url		: url,
+			data	: data,
+			dataType: options.ajax.dataType,
+			options: options,
+			beforeSend: function(){
+				return options.ajax.beforeSend();
+			}
+		})
+		.done(function(result){ options.ajax.success(result);})
+		.fail(function(jqXHR, textStatus){ options.ajax.fail(jqXHR, textStatus);})
+		.always(function(result){options.ajax.complete(result);});	
+	},
+	init = function(){
 			//console.log(obje.nodeType == 1)
 			if(obje.tagName =='FORM'){
 				$.hsnValidate.reset($(obje).find('input[type="text"],textarea,input[type="checkbox"]'));
 				$(obje).find('.required').each(function(i,name) {
-					if($(name).attr('type')=='checkbox'){kontrol.checkbox.secim(name);}
-					else{kontrol.bosluk(name);}
+					if($(name).attr('type')=='checkbox'){check.checkbox.checked(name);}
+					else{check.space(name);}
 				});
 				$(obje).find('.email').each(function(i,name) {;
-					kontrol.mail(name);
+					check.mail(name);
 				});
 				$(obje).find('.number').each(function(i,name) {
-					kontrol.numara(name);
+					check.number(name);
 				});
-				if(name = ayarlar.checkbox.name){
+				if(name = options.checkbox.name){
 					var cbs = $(obje).find('input[type="checkbox"][name="'+name+'"]');
-					kontrol.checkbox.limit(cbs);
+					check.checkbox.limit(cbs);
 				};
 			}else{
 				$.hsnValidate.reset($(obje));
 				var name;
 				if($(obje).hasClass('required')){
-					if($(obje).attr('type')=='checkbox'){kontrol.checkbox.secim(obje);}
-					else{kontrol.bosluk(obje);}
+					if($(obje).attr('type')=='checkbox'){check.checkbox.checked(obje);}
+					else{check.space(obje);}
 				}
 				if ($(obje).hasClass('email')){
-					kontrol.mail(obje);
+					check.mail(obje);
 				}
 				if($(obje).hasClass('number')){
-					kontrol.numara(obje);
+					check.number(obje);
 				}
-				if((name = ayarlar.checkbox.name) && obje.type == 'checkbox'){
+				if((name = options.checkbox.name) && obje.type == 'checkbox'){
 					var cbs = $(obje).parents('form').find('input[type="checkbox"][name="'+name+'"]');
 					$.hsnValidate.reset(cbs);
-					kontrol.checkbox.limit(cbs);
+					check.checkbox.limit(cbs);
 				};
+				handler = true;
 			}
 			if(handler){return false;}
-				else{
-					if(ayarlar.fonksiyon){
-						ayarlar.fonksiyon()
-						return false;
-					}else{return;}
+			else{
+				/*if(options.onCompleteFunc){
+					options.onCompleteFunc()
+					return false;
+				}else{return;}*/
+				if(options.ajax.call){
+					ajax();
+					return false;	
 				}
+			}
 	};
 	$.fn.hsnValidate = function(ayar){
-		ayarlar = $.extend({},ayarlar,ayar);
+		options = $.extend(true,{},defaults,ayar);
 		$(this).submit(function(e){
 			e.preventDefault();
 			obje = this;
-			basla();
+			init();
 		});
-		if(ayarlar.blurTetikleme){
+		if(options.blurTrigger){
 			$(this).find('input[type="text"],input[type="checkbox"],textarea').on('blur',function(){
 				obje = this;
-				basla();
+				init();
 			});
 		};
 	};	
@@ -161,12 +195,13 @@
 		//ilerde kullanırız
 	};
 	$.hsnValidate.reset = function(_inp) {
-		handler = false;
+		//handler = false;
 		_inp.each(function(index, name) {
-			$(name).parent().children('.'+ayarlar.hataClass+'').remove();
+			_window.close(name)
+			//$(name).parent().children('.'+options.errorClass+'').remove();
 		});	
 	};
-	$.hsnValidate.baslangic = function() {
+	$.hsnValidate.init = function() {
 		//Reset Butonuna Tıklanınca Sıfırlama
 		$('input[type="reset"]').click(function(e) {
     		$.hsnValidate.reset($(this).parents('form').find('input[type="text"],textarea,input[type="checkbox"]'));
@@ -178,8 +213,6 @@
 		});
 	};
     $(document).ready(function() {
-    	$.hsnValidate.baslangic();
-    });
-	
-	
+    	$.hsnValidate.init();
+    });	
 })(jQuery);
