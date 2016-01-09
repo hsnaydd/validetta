944f02cb3fc0061e32eae6c84a6aa69ea1bf3519
Hasan Aydoğdu <hasan_aydogdu@windowslive.com>
2016-01-09 03:13:29 +0200

    Fixed #38
    Fixed the problem that occur when using square brackets in input
    elements.

--- src/validetta.js
+++ src/validetta.js
@@ -6,7 +6,7 @@
   var Validetta = {}, // Plugin Class
     FIELDS = {}, // Current fields/fields
     // RegExp for input validate rules
-    RRULE = new RegExp( /^(minChecked|maxChecked|minSelected|maxSelected|minLength|maxLength|equalTo|different|regExp|remote|callback)\[(\w{1,15})\]/i ),
+    RRULE = new RegExp( /^(minChecked|maxChecked|minSelected|maxSelected|minLength|maxLength|equalTo|different|regExp|remote|callback)\[([\w\[\]]{1,25})\]/i ),
     // RegExp for mail control method
     // @from ( http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29 )
     RMAIL = new RegExp( /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ ),
