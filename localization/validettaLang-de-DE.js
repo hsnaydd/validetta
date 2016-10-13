ce7c853bc62fd6be81ccc8269af5efc76126fc60
dcdieci <dcdieci@yahoo.de>
2016-10-13 16:33:03 +0200

    Create validettaLang-de-DE.js
    Just a translation into german

---
+++ localization/validettaLang-de-DE.js
@@ -0,0 +1,22 @@
+(function($){
+	$.fn.validettaLanguage = {};
+	$.validettaLanguage = {
+		init : function(){
+			$.validettaLanguage.messages = {
+				required	: 'Feld wird benötigt.',
+				email		: 'E-Mail nicht gültig.',
+				number		: 'Nur Nummern werden akzeptiert.',
+				maxLength	: 'Maximal {count} Zeichen.',
+				minLength	: 'Mindestens {count} Zeichen.',
+				maxChecked	: 'Maximal {count} Optionen zur Auswahl.',
+				minChecked	: 'Mindestens {count} Optionen auswählen.',
+				maxSelected	: 'Maximal {count} Optionen erlaubt.',
+				minSelected	: 'Mindestens {count} Optionen wählen.',
+				notEqual	: 'Die Eingaben müssen identisch sein.',
+				different   : 'Die Eingaben dürfen nicht identisch sein',
+				creditCard	: 'Kreditkartennummer nicht gültig.'
+			};
+		}
+	};
+	$.validettaLanguage.init();
+})(jQuery);
