e858eb95d6328e2635da286d843cce28aec1e1f0
Hasan Aydoğdu <hsnaydd@gmail.com>
2017-02-18 23:19:35 +0300

    localization files formated and fixed eslint errors

--- localization/validettaLang-pt-BR.js
+++ localization/validettaLang-pt-BR.js
@@ -1,16 +1,16 @@
 (function(){
   window.VALIDETTA_LANGUAGE = {
-    required    : 'Preencha este campo.',
-    email       : 'Insira um endereço de e-mail válido.',
-    number      : 'Insira apenas números.',
-    maxLength   : 'Insira no máximo {count} caracteres.',
-    minLength   : 'Insira no mínimo {count} caracteres.',
-    maxChecked  : 'Marque no máximo {count} opções.',
-    minChecked  : 'Marque no mínimo {count} opções.',
-    maxSelected : 'Selecione no máximo {count} itens',
-    minSelected : 'Selecione no mínimo {count} itens',
-    notEqual    : 'Os campos não são iguais.',
-    different   : 'Fields cannot be the same as each other',
-    creditCard  : 'Número do cartão de crédito inválido.'
+    required: 'Preencha este campo.',
+    email: 'Insira um endereço de e-mail válido.',
+    number: 'Insira apenas números.',
+    maxLength: 'Insira no máximo {count} caracteres.',
+    minLength: 'Insira no mínimo {count} caracteres.',
+    maxChecked: 'Marque no máximo {count} opções.',
+    minChecked: 'Marque no mínimo {count} opções.',
+    maxSelected: 'Selecione no máximo {count} itens',
+    minSelected: 'Selecione no mínimo {count} itens',
+    notEqual: 'Os campos não são iguais.',
+    different: 'Fields cannot be the same as each other',
+    creditCard: 'Número do cartão de crédito inválido.'
   };
 })();
